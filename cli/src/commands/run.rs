eecd4d32cd0ecc5a4eb72e3aac71ce7e3a73c0ef
malezjaa <poreba.kris@gmail.com>
2024-10-18 18:17:01 +0200

    uncomment run_command code

--- cli/src/commands/run.rs
+++ cli/src/commands/run.rs
@@ -17,36 +17,31 @@ pub fn run_cmd() -> Command {
 
 pub fn run_command(ctx: &mut GlobalContext) -> Result<()> {
     ctx.load_config()?;
-
-    // let (lib_dir, modules) = ensure_lib_dir()?;
-
     let path = ctx.get_main_file()?;
 
-    // let content = read_to_string(&path)?;
+    let (lib_dir, modules) = ensure_lib_dir()?;
+    let content = read_to_string(&path)?;
 
-    // let ctx = Context::default();
-    // let source = Source::from_string(content.clone()).with_path(path);
-    // let vm = &mut VM::new();
-    // let module = Module::new(source);
+    let ctx = Context::default();
+    let source = Source::from_string(content.clone()).with_path(path);
+    let vm = &mut VM::new();
+    let module = Module::new(source);
 
-    // for mod_name in modules {
-    //     let path = lib_dir.join(&mod_name).with_extension("roan");
+    for mod_name in modules {
+        let path = lib_dir.join(&mod_name).with_extension("roan");
 
-    //     let content = read_to_string(&path)?;
-    //     let source = Source::from_string(content.clone()).with_path(canonicalize_path(path)?);
-    //     let module = Module::new(source);
+        let content = read_to_string(&path)?;
+        let source = Source::from_string(content.clone()).with_path(canonicalize_path(path)?);
+        let module = Module::new(source);
 
-    //     let module_name = format!("std::{}", mod_name);
-    //     ctx.module_loader.insert(module_name, module);
-    // }
+        let module_name = format!("std::{}", mod_name);
+        ctx.module_loader.insert(module_name, module);
+    }
 
-    // match ctx.eval(module, vm) {
-    //     Ok(_) => Ok(()),
-    //     Err(e) => {
-    //         print_diagnostic(e, Some(content));
-    //         Ok(())
-    //     }
-    // }
-
-    Ok(())
+    match ctx.eval(module, vm) {
+        Ok(_) => Ok(()),
+        Err(e) => Ok({
+            print_diagnostic(e, Some(content));
+        })
+    }
 }
